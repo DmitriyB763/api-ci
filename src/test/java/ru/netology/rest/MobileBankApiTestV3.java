67ab44461fa826b3fc0a50bbdca9ccc5112fc87f
DmitriyB763 <mr.diy3292@gmail.com>
2022-12-27 20:44:51 +0400

    update currency_meaning

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -23,7 +23,7 @@ class MobileBankApiTestV3 {
                 // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
+                .body("[0].currency", equalTo("RUR"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
         ;
     }
